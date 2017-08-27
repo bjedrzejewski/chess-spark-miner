19f351300e053e689a5bce2b4aff18b6a85e4708
bartoszjedrzejewski <jedrus07@gmail.com>
2017-08-27 15:23:20 +0100

    Record set is split by game

--- src/main/java/ChessSparkMiner.java
+++ src/main/java/ChessSparkMiner.java
@@ -1,25 +1,31 @@
 import org.apache.spark.SparkConf;
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.SparkSession;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
 
 public class ChessSparkMiner {
     public static void main(String[] args) {
-        String pngFile = "/Users/bartoszjedrzejewski/github/chesssparkminer/lichess_db_standard_rated_2013-01.pgn"; // Should be some file on your system
-        SparkSession spark = SparkSession.builder()
-                .appName("Chess Spark Miner")
-                .config(new SparkConf().setMaster("local[2]"))
-                .getOrCreate();
-        Dataset<String> logData = spark.read().textFile(pngFile).cache();
+        String pgnFile = "/Users/bartoszjedrzejewski/github/chesssparkminer/lichess_db_standard_rated_2013-01.pgn"; // Should be some file on your system
+        SparkConf conf = new SparkConf()
+                .setAppName("Chess Spark Miner")
+                .setMaster("local[2]");
 
-        long whiteWin = logData.filter(s -> s.contains("1-0")).count();
-        long blackWin = logData.filter(s -> s.contains("0-1")).count();
-        long draw = logData.filter(s -> s.contains("1/2-1/2")).count();
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        sc.hadoopConfiguration().set("textinputformat.record.delimiter", "[Event");
+        JavaRDD<String> pgnData = sc.textFile(pgnFile);
 
+        pgnData = pgnData.filter(line -> line.length() > 1);
+
+        long records = pgnData.count();
+        long whiteWin = pgnData.filter(s -> s.contains("1-0")).count();
+        long blackWin = pgnData.filter(s -> s.contains("0-1")).count();
+        long draw = pgnData.filter(s -> s.contains("1/2-1/2")).count();
+
+        System.out.println("Processed games: " + records);
         System.out.println("White wins: " + whiteWin);
         System.out.println("Black wins: " + blackWin);
         System.out.println("Draw: " + draw);
 
-        spark.stop();
+        sc.stop();
     }
 
 }
